574e15ec518d3807b69a584793c2ac2340e01da7
kklee0930 <kklee0930@gmail.com>
2023-09-22 23:41:41 +0900

    jpa와 db설정, 동작확인

---
+++ jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext // 엔티티 매니저를 주입받는다.
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member); // 데이터베이스에 member 객체를 저장한다. persist: 영속화
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id); // em.find
+    }
+}
---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional // Test에 끝난 후에 DB를 롤백한다.
+//@Rollback(false) // Transactional을 통한 롤백을 하지 않는다.
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+    
+    @Test
+    public void testMember() throws Exception {
+
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+//        Member findMember = memberRepository.find(savedId);
+        assertEquals(findMember.getId(), member.getId());
+        assertEquals(findMember.getUsername(), member.getUsername());
+        assertEquals(findMember, member);
+        System.out.println(findMember == member);
+    }
+}
